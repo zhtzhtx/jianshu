f26a7b7d794a845bae4e1b73f9ecf4ff0f3cd266
hanting <790536079@qq.com>
2021-12-07 10:19:19 +0800

    上传项目

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { container } from './Style';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Title from './components/Title';
 
 function App() {
+  const [type, setType] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={container}>
+      <Title type={type} changeType={type => setType(type)} />
+      {type === 0
+        ? <SignIn />
+        : <SignUp />}
+
     </div>
   );
 }
